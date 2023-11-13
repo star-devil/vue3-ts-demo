b1eca7a3031e9e1e4e9c1fffafa9c26c7fe4968b
star-devil <707298014@qq.com>
2023-11-13 13:36:48 +0800

    feat: 增加css书写顺序规则

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -36,15 +36,107 @@ module.exports = {
         'no-empty-source': null, // 关闭禁止空源码
         'selector-class-pattern': null, // 关闭强制选择器类名的格式
         'property-no-unknown': null, // 禁止未知的属性(true 为不允许)
-        'block-opening-brace-space-before': 'always', //大括号之前必须有一个空格或不能有空白符
         'value-no-vendor-prefix': null, // 关闭 属性值前缀 --webkit-box
         'property-no-vendor-prefix': null, // 关闭 属性前缀 -webkit-mask
+        'block-no-empty': true, //禁止空块
+        "at-rule-no-unknown": null, //不验证@未知的名字,为了兼容scss的函数
+        "comment-no-empty": true, // 禁止空注释
+        "shorthand-property-no-redundant-values": true, //禁止简写属性的冗余值
         'selector-pseudo-class-no-unknown': [
             // 不允许未知的选择器
-            true,
-            {
+            true, {
                 ignorePseudoClasses: ['global', 'v-deep', 'deep'], // 忽略属性，修改element默认样式的时候能使用到
-            },
+            }
         ],
+        "selector-type-no-unknown": [
+            //兼容自定义标签名
+            true, {
+                "ignoreTypes": []
+            }
+        ],
+        "selector-pseudo-element-no-unknown": [
+            // 忽略伪类选择器::v-deep
+            true, {
+                "ignorePseudoElements": ["v-deep"]
+            }
+        ]
     },
+    'order/properties-order': [
+        'position',
+        'top',
+        'right',
+        'bottom',
+        'left',
+        'z-index',
+        'display',
+        'justify-content',
+        'align-items',
+        'float',
+        'clear',
+        'overflow',
+        'overflow-x',
+        'overflow-y',
+        'margin',
+        'margin-top',
+        'margin-right',
+        'margin-bottom',
+        'margin-left',
+        'padding',
+        'padding-top',
+        'padding-right',
+        'padding-bottom',
+        'padding-left',
+        'width',
+        'min-width',
+        'max-width',
+        'height',
+        'min-height',
+        'max-height',
+        'font-size',
+        'font-family',
+        'font-weight',
+        'border',
+        'border-style',
+        'border-width',
+        'border-color',
+        'border-top',
+        'border-top-style',
+        'border-top-width',
+        'border-top-color',
+        'border-right',
+        'border-right-style',
+        'border-right-width',
+        'border-right-color',
+        'border-bottom',
+        'border-bottom-style',
+        'border-bottom-width',
+        'border-bottom-color',
+        'border-left',
+        'border-left-style',
+        'border-left-width',
+        'border-left-color',
+        'border-radius',
+        'text-align',
+        'text-justify',
+        'text-indent',
+        'text-overflow',
+        'text-decoration',
+        'white-space',
+        'color',
+        'background',
+        'background-position',
+        'background-repeat',
+        'background-size',
+        'background-color',
+        'background-clip',
+        'opacity',
+        'filter',
+        'list-style',
+        'outline',
+        'visibility',
+        'box-shadow',
+        'text-shadow',
+        'resize',
+        'transition'
+    ],
 }
